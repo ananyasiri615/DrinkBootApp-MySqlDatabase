bcf5773c1c7a7865d4caa3e6c16755ff4f7bad80
Ananya R Siri <ananyasiri615@gmail.com>
2023-08-01 11:09:03 +0530

    Initial Commit

---
+++ drink-boot-app-mysql/src/main/java/com/ars/DrinkBootAppMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.ars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkBootAppMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkBootAppMysqlApplication.class, args);
+	}
+
+}
---
+++ drink-boot-app-mysql/src/test/java/com/ars/DrinkBootAppMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkBootAppMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
